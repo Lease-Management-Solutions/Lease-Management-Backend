b7a2b4312d1d454a9d6c2ebc30bec025cbfbdf5b
Willians Henrique <willianshenrique94@gmail.com>
2025-02-17 15:46:55 -0300

    fixed bug in validations

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -11,22 +11,9 @@ export const addUser = async (req: Request, res: Response) => {
 
     try {
         
-        const { name, email, password, role } = req.body;
+        const { name, email, password, role, avatar } = req.body;
 
-
-        // Verifica se todos os campos foram preenchidos
-        if (!name || !email || !password || !role) {
-            return res.status(400).json({ message: "Todos os campos são obrigatórios." });
-        }
-        // Verifica se o usuário já existe pelo email
-        const hasUser = await User.findOne({ email });
-        if (hasUser) {
-            return res.status(400).json({ message: "Usuário já cadastrado." });
-        }
-
-        // Criptografa a senha
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-
+        const hashedPassword = await bcrypt.hash(password, saltRounds);          // Criptografa a senha
 
         // Cria o usuário
         const newUser = new User();
@@ -34,6 +21,8 @@ export const addUser = async (req: Request, res: Response) => {
             newUser.email = email,
             newUser. password = hashedPassword,
             newUser.role = role
+            newUser.avatar = avatar
+            newUser.status = 'active'
         
         // Salva o novo usuário no banco de dados
         await newUser.save(); 
@@ -45,10 +34,37 @@ export const addUser = async (req: Request, res: Response) => {
 };
 
 export const users = async (req: Request, res: Response) => {
+    try {
+        const { status } = req.query;
 
-        
-    const usersList = await User.find({});
-    res.status(201).json({ message: 'lista de usuários!', user: usersList });
+        // Condição para filtrar por status, se fornecido
+        const filter = status ? { status } : {};
 
+        const usersList = await User.find(filter);
+
+        res.status(200).json({ message: 'Lista de usuários!', users: usersList });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erro ao recuperar usuários', error });
+    }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findById(id);
+
+        if (!user) {
+            res.status(404).json({ message: 'Usuário não encontrado' });
+            return;
+        }
         
-}
+
+        res.status(200).json({ message: 'Usuário encontrado', user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erro ao buscar o usuário', error });
+    }
+};
+
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,21 +1,14 @@
 import express from 'express'
-import { addUser, users } from '../controllers/user.controller';
+import { addUser, users, getUserById } from '../controllers/user.controller';
 
 
 const router = express.Router();                                //cria um roteador
 
 router.get('/', users);
+router.get('/:id', getUserById);
 
-router.get('/ping', (req, res) => {
-    res.json({pong: true});
-});
 
-router.get('/:id', (req, res) => {
-    const {id} = req.params;
-    res.json({id, name: 'nome da pessoa', idade: 20})
-});
 
-// Rota para criar um novo usuário
 router.post('/', addUser );
 
 export default router;
