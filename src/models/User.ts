3ae95795654cb89712025880b60f79e642574bc3
Willians Henrique <willianshenrique94@gmail.com>
2025-02-17 15:46:07 -0300

    add avatar and status values

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,14 +4,20 @@ interface UserType extends Document {
     name: string;
     email: string;
     password: string;
-    role: string;
+    role: "Corretor" | "Administrativo" | "Financeiro" | "SuperUsuario";
+    avatar?: string;
+    status: "active" | "inactive";
+    
 }
 
 const schema = new Schema<UserType>({
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    role: { type: String, required: true }
+    role: { type: String, required: true, enum: ["Corretor", "Administrativo", "Financeiro", "SuperUsuario"] },
+    avatar: { type: String },
+    status: { type: String, enum: ["active", "inactive"], default: "active" } 
+
 });
 
 const modelName = 'User';
