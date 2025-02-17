b65b1f31698071f4591471d19bc12517cae20055
Willians Henrique <willianshenrique94@gmail.com>
2025-02-17 13:53:19 -0300

    added cryptbcrypt to user password

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,21 +1,42 @@
 import { Request, Response } from "express";
 import User from '../models/User'
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const saltRounds = parseInt(process.env.SALT_ROUNDS || "10");
 
 export const addUser = async (req: Request, res: Response) => {
 
     try {
-
+        
         const { name, email, password, role } = req.body;
 
-        const newUser = new User();
 
+        // Verifica se todos os campos foram preenchidos
+        if (!name || !email || !password || !role) {
+            return res.status(400).json({ message: "Todos os campos são obrigatórios." });
+        }
+        // Verifica se o usuário já existe pelo email
+        const hasUser = await User.findOne({ email });
+        if (hasUser) {
+            return res.status(400).json({ message: "Usuário já cadastrado." });
+        }
+
+        // Criptografa a senha
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+
+        // Cria o usuário
+        const newUser = new User();
             newUser.name = name,
             newUser.email = email,
-            newUser. password = password,
+            newUser. password = hashedPassword,
             newUser.role = role
         
-
-        await newUser.save(); // Salva o novo usuário no banco de dados
+        // Salva o novo usuário no banco de dados
+        await newUser.save(); 
         res.status(201).json({ message: 'Usuário criado com sucesso!', user: newUser });
     } catch (error) {
         console.error(error);
