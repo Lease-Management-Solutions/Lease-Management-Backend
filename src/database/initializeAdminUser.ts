2b92a25b036b929cf3a190f4f5b580362808948b
willians henrique <willianshenrique94@gmail.com>
2025-02-22 09:18:24 -0300

    chenge initial dadas as env

--- src/database/initializeAdminUser.ts
+++ src/database/initializeAdminUser.ts
@@ -1,19 +1,25 @@
 import bcrypt from 'bcrypt';
 import UserModel from '../models/User';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 export async function initializeAdminUser() {
     const existingUser = await UserModel.findOne();
     if (!existingUser) {
-      const hashedPassword = await bcrypt.hash("admin", 10);
+      const userEmail = process.env.USER_EMAIL as string;
+      const userPassword = process.env.USER_PASSWORD as string;
+      const salt = Number(process.env.SALT_ROUNDS);
+      const name = process.env.USER_NAME;
+      const hashedPassword = await bcrypt.hash(userPassword, salt);
       await UserModel.create({
-        name: "Admin", // Adicionado
-        email: "admin@admin.com",
-        password: hashedPassword,
-        role: "SuperUsuario", // Corrigido para a forma exata do enum
+
+        name: name,
+        email: userEmail,
+        password: hashedPassword, 
+        role: "SuperUsuario", 
         status: "active",
       });
       console.log("Usuário admin criado com sucesso.");
     }
   }
-  
